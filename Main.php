112bb0b9c381ba43c479762cd0e43fea64878d05
punkholic <nirajghimirexyz@gmail.com>
2021-09-03 23:24:32 +0545

    changes

--- Main.php
+++ Main.php
@@ -21,16 +21,15 @@ class Main{
         if( ! is_dir( $project_path ) ) {
             echo shell_exec( "composer create-project --prefer-dist laravel/laravel " . $project_path ) ;
         }
-        echo shell_exec( 'sudo chmod -R 777 ' . $project_path );
+        echo shell_exec( 'chmod -R 777 ' . $project_path );
 
-        $this->override_env();
-
-        $this->model = new Model( $this->jsonInput );
-        $this->route = new Route( $this->jsonInput );
+        Common::override_env();        
+        // $this->model = new Model( $this->jsonInput );
+        // $this->route = new Route( $this->jsonInput );
         $this->controller = new Controller( $this->jsonInput );
 
-        $this->migration = new Migration( $this->jsonInput );
-        
+        // $this->migration = new Migration( $this->jsonInput );
+
     }
     
     public function processEnv(){
@@ -46,54 +45,6 @@ class Main{
     public function testing(){
         // new Controller($this->jsonInput);
         // $this->model->processFile();
-    }
-
-    public function override_env()
-    {
-        $new_env = file_get_contents(".env");
-        preg_match_all( '/([\w\S ]+)=([\w\S ]+)/s' , $new_env , $overwrite);
-
-        // preg_match_all( '/([\w\S ]+)=([\w\S ]+)|([\w\S ]+)=/s' , $new_env, $overwrite);
-
-        
-        $data = file_get_contents( PROJECT_PATH . "/.env" );
-        preg_match_all( '/([\w\S ]+)=([\w\S ]+)/s' , $data, $matches);
-        // preg_match_all( '/([\w\S ]+)=([\w\S ]+)|([\w\S ]+)=/s' , $data, $matches);
-
-     
-
-        $new_env_overwrite = ""; 
-        $new_env_overwrite .= "\n ";
-
-        for ( $i = 0 ; $i < count( $matches[1] ) ; $i++ ) {
-            $matched_checker = FALSE;
-            for ( $j = 0 ; $j < count ( $overwrite[1] ) ; $j++ )
-            {
-                if( $matches[ 1 ][ $i ] == $overwrite[ 1 ][ $j ] )
-                {
-                    $matches[ 2 ][ $i ] = $overwrite[ 2 ][ $j ];
-                    $new_env_overwrite .= trim( $matches[ 1 ][ $i ] ) . '=' . trim( $overwrite[ 2 ][ $j ] ) ;
-                    $new_env_overwrite .= "\n ";
-                    $matched_checker = TRUE ;
-                }
-               
-            }
-            if( ! $matched_checker )
-            {
-                $new_env_overwrite .= trim( $matches[ 1 ][ $i ] ) . '=' . trim( $matches[ 2 ][ $i ] );
-                $new_env_overwrite .= "\n ";
-            }
-            if( $i % 4 == 0)
-            {
-                $new_env_overwrite .= "\n ";
-
-            }
-
-        }
-        
-        file_put_contents( PROJECT_PATH . "/.env", $new_env_overwrite  );
-       
-        
     }
     
 }
