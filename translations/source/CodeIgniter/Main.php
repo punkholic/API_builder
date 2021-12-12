849f35c38ef88798c06439f6b4ed4e01a1fbecd7
punkholic <nirajghimirexyz@gmail.com>
2021-12-13 00:30:31 +0545

    laravel fix

--- translations/source/CodeIgniter/Main.php
+++ translations/source/CodeIgniter/Main.php
@@ -1,8 +1,12 @@
 <?php 
 class Main{
-    public function __construct(){
-        echo "hello world";
+    public function __construct($jsonData){
+        $this->jsonInput = json_decode( file_get_contents( $jsonData ) );
+            $this->route = new Route( $this->jsonInput );
+
     }
+
 }
-$main = new Main();
+$json_dir = __DIR__ . "/../../../input.json";
+$main = new Main($json_dir);
 ?>
---
+++ translations/source/CodeIgniter/classes/Route.php
@@ -0,0 +1,41 @@
+<?php 
+class Route{
+
+    public function __construct($jsonData){
+        $this->jsonInput = $jsonData;
+        $this->routeTo = ["view", "add", "edit", "delete"];
+        $this->filePath = __DIR__ . PROJECT_PATH . "/routes/api.php";
+        $this->processRoute();
+    }
+
+    public function processRoute(){
+        foreach( $this->jsonInput->data as $data ) {
+            $this->processEach($data);
+        }
+    }
+
+    public function processEach( $modelData ){
+
+        $toWrite = file_get_contents( $this->filePath );
+       
+        foreach($this->routeTo as $i){
+            foreach($modelData->model->$i as $j){
+
+                $type = strtolower($j->request->type);
+                $route = $j->request->route;
+                $methodName = $j->request->name;
+
+                //remove if already defined in file
+                $regex = "\$routes->get\([\"'](.*);";
+                $toWrite = preg_replace('/$regex/s', '', $toWrite);
+                
+                // overwrite or add new one
+                $toWrite .= "\nRoute::$type('$route','App\Http\Controllers\\$modelData->controller@$methodName');\n";
+            }
+        }
+        file_put_contents($this->filePath, $toWrite);
+    }
+
+}
+
+?>
